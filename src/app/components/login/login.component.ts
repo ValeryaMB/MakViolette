90a5c86e4377f87509523fa7e248d4cc5ecb4497
Valeria <yokoyamamoto92@gmail.com>
2023-01-15 13:17:00 -0500

    login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  nombre: any
+  correo:any
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  recebird(){
+
+  }
+  navegar(){
+    console.log(this.nombre)
+    console.log(this.correo)
+    if(this.nombre='valeria'&&this.correo=='vale66@gmail.com'){
+      this.router.navigate(['3'])
+    }
+    else{
+      this.router.navigate(['2'])
+    }
+  }
+}
