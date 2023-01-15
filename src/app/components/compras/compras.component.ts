9d94b2ab37748b18e23d9d507e81514ad46a0859
Valeria <yokoyamamoto92@gmail.com>
2023-01-15 15:24:45 -0500

    sjsc

--- src/app/components/compras/compras.component.ts
+++ src/app/components/compras/compras.component.ts
@@ -1,4 +1,6 @@
+import { NgForOf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compras',
@@ -7,9 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComprasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  cantidad:number=0
+  cantidad2:number=0
+  cantidad3:number=0
+  cantidad4:number=0
+  cantidad5:number=0
+  cantidad6:number=0
+  cantidad7:number=0
+  cantidad8:number=0
+  
+  total:number
+  total2:number
+  total3:number
+  total4:number
+  total5:number
+  total6:number
+  total7:number
+  total8:number
+
+  carro:any  
 
   ngOnInit() {
+    this.calcula()
+  }
+
+  navegar(){
+    this.router.navigate([''])
+    this.limpiar()
+  }
+
+  navegar2(){
+    this.router.navigate(['4'])
+  }
+
+  suma(precio:number,nombreproducto:string,cant:number,tot:number){
+    tot=cant*precio
+    this.calcula()
+  }
+
+  resta(precio:number,nombreproducto:string,cant:number,tot:number){
+    tot=cant*precio   
+    this.calcula()
+  }
+
+  compra(precio:number, nombreproducto:string,cant:number,tot:number){    
+    tot=cant*precio
+    localStorage.setItem(nombreproducto,tot.toString()+ '#'+ cant.toString()+'#'+precio.toString())
+    this.calcula()
+  }
+
+  limpiar(){
+    localStorage.clear();
+  }
+
+  calcula() {
+    this.carro=0;
+    for (const key in localStorage) {
+      if (key.startsWith("Producto")) {
+        let valor=localStorage[key];
+        console.log(key, "Dato", valor);
+        let partes=valor.split('#');
+        console.log(partes);
+        let l_total='0';
+        if (partes.length==3){
+          l_total=partes[0];
+        }
+        this.carro = this.carro +  (+l_total);
+      }
+    }
+    console.log(this.carro);
   }
 
 }
